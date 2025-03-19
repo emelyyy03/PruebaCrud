3c6c0cbdc1123ec591ed744d04b88550b61b3b77
Johana Emely Tobar Cruz <emelyyy03@outlook.com>
2025-03-19 08:09:00 -0600

    Agregar archivos de proyecto.

---
+++ PruebaCrud.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PruebaCrud.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
